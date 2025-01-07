8d4c25fc411281a80a12feb8cfcea6b2e9310b91
Ahkeem <langahkeem@gmail.com>
2025-01-07 11:48:12 -0600

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tweet, setTweet] = useState("")
+  const [sentiment, setSentiment] = useState(""); // positive, negative, neutral
 
+  const callOpenAIAPI = async () => {
+    console.log("Calling OpenAI API")
+  }
+console.log("tweet", tweet)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+ <div>
+  <div>
+    <textarea
+      onChange={(e) => setTweet(e.target.value)}
+      placeholder='Paste your tweet here!'
+      cols={50}
+      rows={10}
+    />
+  </div>
+  <div>
+    <button onClick={callOpenAIAPI}>Get The Tweet Sentiment From OpenAI API</button>
+    {sentiment && <h3>This Tweet Is: {sentiment}</h3>}
+  </div>
+ </div>
   )
 }
 
